fb966681be02fb8751108187c2bbf458619d7959
Sakib Hasan <sakib100.sa@gmail.com>
2023-11-25 21:08:44 +0600

    hero section completed

--- client/sections/Hero.jsx
+++ client/sections/Hero.jsx
@@ -9,13 +9,21 @@ const Hero = () => {
                         <h4 className="text-light-primary font-semibold text-[0.8rem] md:text-[1rem] lg:text-[1.25rem]">
                             Design | Development | Marketing{" "}
                         </h4>
-                        <h1 className="text-[2.5rem] md:text-[3rem] lg:text-[3.5rem] font-semibold text-light-text">
+                        <h1 className="text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[3.5rem] font-semibold text-light-text">
                             Unlock the Limitless Potential of Your Brand
                         </h1>
-                        <p className="font-[400] text-[0.8rem] lg:text-[1.2rem] text-light-text">
+                        <p className="font-[400] text-[1rem] lg:text-[1.2rem] text-light-text">
                             Empowering Brands for a Bold Tomorrow: Where Vision
                             Meets Identity
                         </p>
+                        <div className="w-full flex flex-col md:flex-row justify-center md:justify-start items-center gap-5">
+                            <button className="text-[1rem] bg-light-primary rounded-[50px] px-[2em] py-[1em] text-light-background">
+                                Free Consultation
+                            </button>
+                            <button className="text-[1rem] bg-light-secondary rounded-[50px] px-[2em] py-[1em] text-light-text">
+                                Get Connected
+                            </button>
+                        </div>
                     </div>
                 </div>
                 <div className="col-span-1 md:col-span-5 xl:md:col-span-4">
